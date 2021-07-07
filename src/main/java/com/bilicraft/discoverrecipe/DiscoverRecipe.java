3057e6a6b0838b394408192df8d683e6d77e6794
Ghost_chu <2908803755@qq.com>
2021-07-07 14:42:54 +0800

    Add recipes cache

--- src/main/java/com/bilicraft/discoverrecipe/DiscoverRecipe.java
+++ src/main/java/com/bilicraft/discoverrecipe/DiscoverRecipe.java
@@ -1,6 +1,7 @@
 package com.bilicraft.discoverrecipe;
 
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.item.crafting.Recipe;
 import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_17_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
@@ -10,14 +11,26 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.server.PluginEnableEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.Collection;
 
 public final class DiscoverRecipe extends JavaPlugin implements Listener {
+
+    private Collection<Recipe<?>> recipesCache;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        updateRecipesCache();
         Bukkit.getOnlinePlayers().forEach(this::discoverRecipe);
         Bukkit.getPluginManager().registerEvents(this,this);
+        new BukkitRunnable(){
+            @Override
+            public void run() {
+                updateRecipesCache();
+            }
+        }.runTask(this);
     }
 
     @Override
@@ -25,9 +38,13 @@ public final class DiscoverRecipe extends JavaPlugin implements Listener {
         // Plugin shutdown logic
     }
 
+    private void updateRecipesCache(){
+        this.recipesCache = MinecraftServer.getServer().getRecipeManager().getRecipes();
+    }
+
 
     private void discoverRecipe(Player player) {
-        int unlocked = ((CraftPlayer) player).getHandle().awardRecipes(MinecraftServer.getServer().getRecipeManager().getRecipes());
+        int unlocked = ((CraftPlayer) player).getHandle().awardRecipes(recipesCache);
         if (unlocked != 0)
             getLogger().info("Unlocked " + unlocked + " recipes for player " + player.getName());
     }
@@ -39,6 +56,7 @@ public final class DiscoverRecipe extends JavaPlugin implements Listener {
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void pluginLoaded(PluginEnableEvent event) {
+        updateRecipesCache();
         Bukkit.getOnlinePlayers().forEach(this::discoverRecipe);
     }
 }
